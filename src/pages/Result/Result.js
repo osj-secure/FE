a24681af76b6db36b68d92755afef7a9bb1688b6
kmj200392 <kmj200392@gmail.com>
2024-12-07 03:17:10 +0900

    result

--- src/pages/Result/Result.js
+++ src/pages/Result/Result.js
@@ -1,38 +1,33 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { LuRefreshCcw } from "react-icons/lu";
+import { FiRefreshCw } from "react-icons/fi";
 import { IoMdSkipBackward } from "react-icons/io";
 import { LuDownload } from "react-icons/lu";
-import './Result.css'
+import './Result.css';
 
 const Result = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     return (
         <div>
             <div className='top'>
-                <button className = 'back' onClick={() => {navigate("/main")}}>
-                    <IoMdSkipBackward/>
+                <button className='back' onClick={() => {navigate("/main")}}>
+                    <IoMdSkipBackward className = 'back_icon'/>
                     back
                 </button>
                 <button className='refresh'>
-                    <LuRefreshCcw style={{width: '5vh', height: '5vh'}}/>
+                    <FiRefreshCw style={{width: '5vh', height: '5vh'}}/>
                 </button>
             </div>
             <div className='middle'>
-                <button className='result'>
-                    <img src="/logo192.png" alt = 'logo'></img>
-                </button>
-                <button className='result'>
-                    <img src="/logo192.png" alt = 'logo'></img>
-                </button>
-                <button className='result'>
-                    <img src="/logo192.png" alt = 'logo'></img>
-                </button>
+                <div className='result'></div>
+                <div className='result'></div>
+                <div className='result'></div>
             </div>
             <div className='bottom'>
                 <button className='download'>
-                    <LuDownload/>
+                    <LuDownload className='download_icon'/>
+                    Download
                 </button>
             </div>
         </div>
