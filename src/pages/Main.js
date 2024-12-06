249bbc7a0c857ee8efaef8ecf1d316fef4d4fcf2
myeonghah <myeongha159357@gmail.com>
2024-12-06 22:31:07 +0900

    first commit

---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <div>
+            <h1>Main Page</h1>
+            <p>This is main page.</p>
+        </div>
+    );
+}
+
+export default Main;
