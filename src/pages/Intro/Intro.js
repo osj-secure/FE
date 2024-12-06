b4b8af23dd00c566d9bcaa1890b1b8baa4612198
kmj200392 <kmj200392@gmail.com>
2024-12-07 01:30:42 +0900

    a4

--- src/pages/Intro/Intro.js
+++ src/pages/Intro/Intro.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
-function Intro() {
+const Intro = () => {
     const navigate = useNavigate();
     return (
         <div>
