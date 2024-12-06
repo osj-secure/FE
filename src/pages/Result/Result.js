3431e30a1ec5ae059ac7b78032c8b396dc325bab
kmj200392 <kmj200392@gmail.com>
2024-12-07 01:24:50 +0900

    check

--- src/pages/Result/Result.js
+++ src/pages/Result/Result.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { LuRefreshCcw } from "react-icons/lu";
+import { IoMdSkipBackward } from "react-icons/io";
+import { LuDownload } from "react-icons/lu";
+import './Result.css'
 
-function Result() {
+const Result = () => {
+    const navigate = useNavigate()
+
     return (
         <div>
-            <h1>Result</h1>
-            <p>This is result page.</p>
+            <div className='top'>
+                <button className = 'back' onClick={() => {navigate("/main")}}>
+                    <IoMdSkipBackward/>
+                    back
+                </button>
+                <button className='refresh'>
+                    <LuRefreshCcw/>
+                </button>
+            </div>
+            <div className='middle'>
+                <button className='result'>
+                    <img src="/logo192.png" alt = 'logo'></img>
+                </button>
+                <button className='result'>
+                    <img src="/logo192.png" alt = 'logo'></img>
+                </button>
+                <button className='result'>
+                    <img src="/logo192.png" alt = 'logo'></img>
+                </button>
+            </div>
+            <div className='bottom'>
+                <button className='download'>
+                    <LuDownload/>
+                </button>
+            </div>
         </div>
     );
 }
